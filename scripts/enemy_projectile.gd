e7bca75ab0a04fdea68fa532be6f9d5f01427248
dfarmer7908-lab <dfarmer7908@student.pps.net>
2025-10-03 10:01:13 -0700

    make turret fire

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -1,7 +1,20 @@
 extends Area2D
+var direction
+var speed=300
+
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
-func _on_body_entered(body):
+func _on_body_entered (body):
+	if body.name == "Player":
+		body.change_health(-2)
+		queue_free()
+
 	pass
+func _physics_process(_delta):
+	position += direction * speed * _delta
+	
+	
+func set_direction(target):
+	direction = position.direction_to(target)
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,21 +1,35 @@
 extends CharacterBody2D
-
-
+var projectile_original = preload("res://scenes/enemy_projectile.tscn")
+var start_time = 2
+var timer = start_time
+var in_range = false
+var player
 func _ready():
 	
 	pass
 
+
+
 func _process(delta: float) -> void:
+	if in_range:
+		timer -= delta
+	if timer < 0:
+		shoot(player)
+		timer = start_time
 	pass
 
 
-	
-
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	
+	if body.name == "Player":
+		player = body
+		in_range = true
 	pass # Replace with function body.
-
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	pass # Replace with function body.
+func shoot(target):
+	var projectile_clone = projectile_original.instantiate()
+	projectile_clone.global_position = position
+	projectile_clone.set_direction(target.position)
+	get_tree().get_root().add_child(projectile_clone)
+	
